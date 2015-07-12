a426e47e9cb97cf43d1db24021996b299d5ea15c
Erik Schneider <erik.schneider41@gmail.com>
2015-07-12 11:33:58 -0500

    Starts adding templates

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 var express = require('express');
+var expressHandlebars = require('express-handlebars');
+
 var app = express();
 
+app.engine('handlebars', expressHandlebars({
+  defaultLayout: 'main',
+  partialsDir: './views/partials/'
+}));
+
+app.set('view engine', 'handlebars');
+
 app.get('/', function (req, res) {
-  res.send('Hello World');
+  res.render('index');
 });
 
 app.listen(3000);

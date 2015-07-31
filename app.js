08b825efadf0bbfab904e52c3714f7e36ba624cf
Braintree <code@getbraintree.com>
2015-07-31 14:47:07 +0000

    WIP cart

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var expressHandlebars = require('express-handlebars');
+var cookieParser = require('cookie-parser');
 var database = require('./lib/database');
 var templateHelpers = require('./lib/template-helpers');
 
@@ -12,22 +13,28 @@ app.engine('handlebars', expressHandlebars({
 
 app.set('view engine', 'handlebars');
 
+app.use(cookieParser());
+
 app.get('/', function (req, res) {
   var products = database.getProducts();
+  var cart = req.cookies.easyCart;
 
   res.render('index', {
     title: 'Easy Press',
     products: products,
+    cart: cart,
     helpers: templateHelpers
   });
 });
 
 app.get('/products/:slug', function (req, res) {
   var product = database.getProduct(req.params.slug);
+  var cart = req.cookies.easyCart;
 
   res.render('product', {
     title: 'Easy Press: ' + product.name,
-    product: product
+    product: product,
+    cart: cart
   });
 });
 

9a6d50e2d72e2ab749b419989da6a4806ecffc03
Erik Schneider <erik.schneider41@gmail.com>
2016-01-30 13:50:17 -0600

    Adds videos and photos and token 3 form

--- assets/js/main.js
+++ assets/js/main.js
@@ -73,3 +73,43 @@ function fetchProductHTML(path) {
       modal.open();
     });
 }
+
+(function() {
+  var images = document.querySelector('.slideshow').getElementsByTagName('img');
+  var numberOfImages = images.length;
+  var i = 1;
+
+// we set the 'fx' class on the first image when the page loads
+  document.querySelector('.slideshow').getElementsByTagName('img')[0].className = 'fx';
+
+// this calls the kenBurns function every 4 seconds
+// you can increase or decrease this value to get different effects
+  window.setInterval(kenBurns, 6000);
+
+// the third variable is to keep track of where we are in the loop
+// if it is set to 1 (instead of 0) it is because the first image is styled when the page loads
+
+  function kenBurns() {
+    if (i === numberOfImages) {
+      i = 0;
+    }
+
+    images[i].className = 'fx';
+
+    // we can't remove the class from the previous element or we'd get a bouncing effect so we clean up the one before last
+    // (there must be a smarter way to do this though)
+    if (i === 0) {
+      images[numberOfImages - 2].className = '';
+    }
+
+    if (i === 1) {
+      images[numberOfImages - 1].className = '';
+    }
+
+    if (i > 1) {
+      images[i - 2].className = '';
+    }
+
+    i++;
+  }
+})();

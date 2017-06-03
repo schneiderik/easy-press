c20739454007f5794361d49d76462b4311aac51e
Erik Schneider <erik.schneider41@gmail.com>
2017-06-03 14:09:04 -0500

    Adds LazyLoad to ProductListing images

--- assets/js/product.js
+++ assets/js/product.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import LazyLoad from 'react-lazy-load';
 import helpers from './../../lib/handlebars-helpers';
 
 class Product extends React.Component {
@@ -10,7 +11,9 @@ class Product extends React.Component {
     return (
       <a className="product" href={'/product/' + this.props.slug}>
 				<div className="product__image-container">
-					<img className="product__image" src={this.props.coverSrc} />
+					<LazyLoad offset={1000}>
+						<img className="product__image" src={this.props.coverSrc} />
+					</LazyLoad>
 				</div>
 				<h1 className="product__title">{this.props.title}</h1>
 				<p className="product__author">{helpers.formatAuthor(this.props.author)}</p>
